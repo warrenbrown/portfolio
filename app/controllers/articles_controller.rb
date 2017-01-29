8da6c3efb2966c04dd7793498cd0dfd0a6b8833f
warrenbrown <warren@katapultmedia.com>
2017-01-29 07:56:25 -0600

    fixed styling issues

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -31,7 +31,7 @@ class ArticlesController < ApplicationController
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
         format.json { render :show, status: :created, location: @article }
       else
-        format.html { render :new }
+        format.html { render :new, alert: 'Article has not been created.'}
         format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
