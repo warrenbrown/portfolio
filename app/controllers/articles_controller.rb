db2b905e1157207b6fce338bac7c0f227b1ad573
warrenbrown <warren@katapultmedia.com>
2017-02-04 09:10:14 -0600

    Fixed styiling
    Corrected styling on Articles index and Projects index pages.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.limit(3).order("CREATED_AT").all
   end
 
   # GET /articles/1
